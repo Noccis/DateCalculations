6437bc8d9ed76224c6d7a9746bac686c99732ac1
Noccis <toniwincent83@gmail.com>
2023-09-15 15:30:58 +0200

    Added string resource for button

--- app/src/main/java/com/example/datecalculations/MainActivity.kt
+++ app/src/main/java/com/example/datecalculations/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.datecalculations.ui.screens.DateCalculationUI
 import com.example.datecalculations.ui.theme.DateCalculationsTheme
--- app/src/main/java/com/example/datecalculations/ui/screens/DateCalculationUI.kt
+++ app/src/main/java/com/example/datecalculations/ui/screens/DateCalculationUI.kt
@@ -2,19 +2,29 @@ package com.example.datecalculations.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.datecalculations.R
 import com.example.datecalculations.model.DateHandler
 
 @Composable
 fun DateCalculationUI() {
 
     var datePickerVisible = false
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(15.dp)) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.open_date_picker_text))
+        }
         if (DateHandler.listOfDates.isNotEmpty()) {
 
             Spacer(modifier = Modifier.padding(10.dp))
