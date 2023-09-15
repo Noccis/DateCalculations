5428bfae50f79a5fb5fc623571f5ba2212028163
Noccis <toniwincent83@gmail.com>
2023-09-15 20:49:19 +0200

    added button to toggle datepicker

--- app/src/main/java/com/example/datecalculations/ui/screens/DateCalculationUI.kt
+++ app/src/main/java/com/example/datecalculations/ui/screens/DateCalculationUI.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,14 +19,32 @@ import com.example.datecalculations.model.DateHandler
 @Composable
 fun DateCalculationUI() {
 
-    var datePickerVisible = false
+    var datePickerVisible by remember {
+        mutableStateOf(false)
+    }
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(15.dp)) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = stringResource(id = R.string.open_date_picker_text))
+            .padding(15.dp)
+    ) {
+
+        if (datePickerVisible) {
+            DatePickerScreen()
+            Button(onClick = {
+                datePickerVisible = false
+            }) {
+                Text(text = stringResource(id = R.string.set_date_text))
+            }
+        } else {
+            Button(onClick = {
+                datePickerVisible = true
+            }) {
+                Text(text = stringResource(id = R.string.open_date_picker_text))
+            }
         }
+
         if (DateHandler.listOfDates.isNotEmpty()) {
 
             Spacer(modifier = Modifier.padding(10.dp))
